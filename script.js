0e834f96257e0a2fc4a90ebfa38c6a9b8d4afba9
Guilherme Argollo <156041698+Gui18120@users.noreply.github.com>
2025-08-26 20:59:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         if (mesesCompletos < 0) {
-            mesesCompletos += 12;
+            mesesCompletos += 0;
             anosCompletos--;
         }
 
